9e06b3f4ada23bb150542f5dafaa2291779d092b
Dhruv Singh Jamwal <142426205+dhruvjamwal@users.noreply.github.com>
2026-03-03 09:41:21 +0530

    Create file.c

---
+++ src/file.c
@@ -0,0 +1,301 @@
+#include<types.h>
+#include<context.h>
+#include<file.h>
+#include<lib.h>
+#include<serial.h>
+#include<entry.h>
+#include<memory.h>
+#include<fs.h>
+#include<kbd.h>
+#include<pipe.h>
+
+void free_file_object(struct file *filep)
+{
+    if(filep)
+    {
+       os_page_free(OS_DS_REG ,filep);
+       stats->file_objects--;
+    }
+}
+
+struct file *alloc_file()
+{
+  struct file *file = (struct file *) os_page_alloc(OS_DS_REG); 
+  file->fops = (struct fileops *) (file + sizeof(struct file)); 
+  bzero((char *)file->fops, sizeof(struct fileops));
+  stats->file_objects++;
+  return file; 
+}
+
+static int do_read_kbd(struct file* filep, char * buff, u32 count)
+{
+  kbd_read(buff);
+  return 1;
+}
+
+static int do_write_console(struct file* filep, char * buff, u32 count)
+{
+  struct exec_context *current = get_current_ctx();
+  return do_write(current, (u64)buff, (u64)count);
+}
+
+struct file *create_standard_IO(int type)
+{
+  struct file *filep = alloc_file();
+  filep->type = type;
+  if(type == STDIN)
+     filep->mode = O_READ;
+  else
+      filep->mode = O_WRITE;
+  if(type == STDIN){
+        filep->fops->read = do_read_kbd;
+  }else{
+        filep->fops->write = do_write_console;
+  }
+  filep->fops->close = generic_close;
+  filep->ref_count = 1;
+  return filep;
+}
+
+int open_standard_IO(struct exec_context *ctx, int type)
+{
+   int fd = type;
+   struct file *filep = ctx->files[type];
+   if(!filep){
+        filep = create_standard_IO(type);
+   }else{
+         filep->ref_count++;
+         fd = 3;
+         while(ctx->files[fd])
+             fd++; 
+   }
+   ctx->files[fd] = filep;
+   return fd;
+}
+
+void do_file_fork(struct exec_context *child)
+{
+    for(int i=0;i<MAX_OPEN_FILES;i++){
+        if(child->files[i]){
+            child->files[i]->ref_count++;
+        }
+    }
+}
+
+void do_file_exit(struct exec_context *ctx)
+{
+    for(int i=0;i<MAX_OPEN_FILES;i++){
+        if(ctx->files[i]){
+            generic_close(ctx->files[i]);
+        }
+    }
+}
+
+long generic_close(struct file *filep)
+{
+    int ret_fd = -EINVAL; 
+    if(filep == NULL){
+        return ret_fd;
+    }
+    if(--(filep->ref_count)){
+      return filep->ref_count;
+    }
+    else{
+        if(filep->type==REGULAR){
+            free_file_object(filep);
+        }
+        else if(filep->type==PIPE){
+            if(filep->mode == O_READ){
+                filep->pipe->is_ropen = 0;
+                if(filep->pipe->is_wopen == 0){
+                    free_pipe_info(filep->pipe);
+                }
+            }
+            else if(filep->mode == O_WRITE){
+                filep->pipe->is_wopen = 0;
+                if(filep->pipe->is_ropen == 0){
+                    free_pipe_info(filep->pipe);
+                }
+            }
+            free_file_object(filep);
+            return 0;
+        }
+        else{
+            free_file_object(filep);
+        }
+        return 0;
+    }
+    return ret_fd;
+}
+
+static int do_read_regular(struct file *filep, char * buff, u32 count)
+{
+    int ret_fd = -EINVAL; 
+    if(filep == NULL || buff == NULL){
+        return ret_fd;
+    }
+    if(filep->mode&0x1){
+        int bytes_read = flat_read(filep->inode, buff, count, &(filep->offp));
+        if( bytes_read < 0 ){
+            return -EINVAL;
+        }
+        filep->offp+=bytes_read;
+        return bytes_read;
+    }
+    else{
+        return -EACCES;
+    }
+    return ret_fd;
+}
+
+static int do_write_regular(struct file *filep, char * buff, u32 count)
+{
+    int ret_fd = -EINVAL; 
+    if(filep == NULL || buff == NULL){
+        return ret_fd;
+    }
+    if(filep->mode&0x2){
+        int bytes_written = flat_write(filep->inode, buff, count, &(filep->offp));
+        if( bytes_written < 0 ){
+            return -EINVAL;
+        }
+        filep->offp+=bytes_written;
+        return bytes_written;
+    }
+    else{
+        return -EACCES;
+    }
+    return ret_fd;
+}
+
+static long do_lseek_regular(struct file *filep, long offset, int whence)
+{
+    int ret_fd = -EINVAL; 
+    if(filep == NULL){
+        return ret_fd;
+    }
+    unsigned int offp;
+    if(whence==SEEK_SET){
+        offp=0; 
+    } 
+    else if(whence==SEEK_CUR){
+        offp=filep->offp;
+    }
+    else if(whence==SEEK_END){
+        offp=filep->inode->file_size;
+    }
+    else{
+        return ret_fd;
+    }
+    offp+=offset;
+    if(offp >= FILE_SIZE || offp < 0){
+        return ret_fd;
+    }
+    filep->offp = offp;
+    return filep->offp;
+}
+
+extern int do_regular_file_open(struct exec_context *ctx, char* filename, u64 flags, u64 mode)
+{ 
+    int ret_fd = -EINVAL; 
+
+    struct inode *file_inode;
+    if(file_inode=lookup_inode(filename)){
+       if((flags&file_inode->mode&0x7)==(flags&0x7)){
+           struct file* file_ptr = alloc_file();
+           file_ptr->type = REGULAR;
+           file_ptr->mode = flags;
+           file_ptr->offp = 0;
+           file_ptr->ref_count = 1;
+           file_ptr->inode = file_inode;
+           file_ptr->fops->read = do_read_regular;
+           file_ptr->fops->write = do_write_regular;
+           file_ptr->fops->lseek = do_lseek_regular;
+           file_ptr->fops->close = generic_close;
+           file_ptr->pipe = NULL;
+           int fd=3;
+           while(ctx->files[fd]){
+              fd++; 
+           }
+           ctx->files[fd]=file_ptr;
+           return fd;
+       }
+       else{
+           return -EACCES; 
+       }
+    }
+    else{
+        if((flags&O_CREAT)==O_CREAT){
+            file_inode = create_inode(filename, mode);
+            struct file* file_ptr = alloc_file();
+            file_ptr->type = REGULAR;
+            file_ptr->mode = flags;
+            file_ptr->offp = 0;
+            file_ptr->ref_count = 1;
+            file_ptr->inode = file_inode;
+            file_ptr->fops->read = do_read_regular;
+            file_ptr->fops->write = do_write_regular;
+            file_ptr->fops->lseek = do_lseek_regular;
+            file_ptr->fops->close = generic_close;
+            file_ptr->pipe = NULL;
+            int fd=3;
+            while(ctx->files[fd]){
+               fd++; 
+               if(fd==MAX_OPEN_FILES){
+                 return -EOTHERS;
+               }
+            }
+            ctx->files[fd]=file_ptr;
+            return fd;
+        }    
+        else{
+            return -EINVAL;
+        }
+    }
+    return ret_fd;
+}
+
+int fd_dup(struct exec_context *current, int oldfd)
+{
+    int ret_fd = -EINVAL; 
+    if(oldfd >= MAX_OPEN_FILES){
+        return -EOTHERS;
+    }
+    if(current->files[oldfd]){
+        int new_fd=3;
+        while(current->files[new_fd]){
+            new_fd++;
+            if(new_fd == MAX_OPEN_FILES){
+                return -EOTHERS;
+            }
+        }
+        current->files[new_fd]=current->files[oldfd];
+        current->files[new_fd]->ref_count++;
+        return new_fd;
+    }
+    else{
+        return -EINVAL;
+    }
+    return ret_fd;
+}
+
+
+int fd_dup2(struct exec_context *current, int oldfd, int newfd)
+{
+    int ret_fd = -EINVAL; 
+    if(oldfd >= MAX_OPEN_FILES || newfd >= MAX_OPEN_FILES){
+        return -EOTHERS;
+    }
+    if(current->files[oldfd]){
+        if(current->files[newfd]&&oldfd!=newfd){
+            current->files[newfd]->fops->close(current->files[newfd]);
+            current->files[oldfd]->ref_count++;
+        }
+        current->files[newfd]=current->files[oldfd];
+        return newfd;
+    }                                
+    else{
+        return -EINVAL;
+    }
+    return ret_fd;
+}
